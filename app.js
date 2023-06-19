66e25a1849344e947f2074054868c274081f73b5
Nikolai Lønberg <niklonberg@gmail.com>
2023-06-19 10:36:41 +0200

    Add if statement to check for same choice

--- app.js
+++ app.js
@@ -9,9 +9,14 @@ function playRound(playerSelection, computerSelection) {
     playerSelection = playerSelection.toLowerCase();
     computerSelection = computerSelection.toLowerCase();
 
+    if (playerSelection === computerSelection) {
+        console.log(`Both of participants used ${playerSelection} - this round is a draw`)
+    }
 }
 
 const playerSelection = "rock"
 const computerSelection = getComputerChoice();
 
 console.log(getComputerChoice());
+
+playRound(playerSelection, computerSelection);
