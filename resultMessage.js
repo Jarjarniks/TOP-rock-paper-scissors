647b63cf60f32d6bbf3281de0577247a539d067c
Nikolai Lønberg <niklonberg@gmail.com>
2023-06-19 13:26:35 +0200

    Fix 3 functions use of classes local variables

--- resultMessage.js
+++ resultMessage.js
@@ -5,15 +5,15 @@ class resultMsg {
     }
 
     win() {
-        return `Player wins! player chose ${playerSelection}, computer chose ${computerSelection}`
+        return `Player wins! player chose ${this.playerSelection}, computer chose ${this.computerSelection}`
     }
     
     draw() {
-        return `Both participants used ${playerSelection} - this round is a draw`
+        return `Both participants used ${this.playerSelection} - this round is a draw`
     }
 
     lose() {
-        return `Player loses! player chose ${playerSelection}, computer chose ${computerSelection}`
+        return `Player loses! player chose ${this.playerSelection}, computer chose ${this.computerSelection}`
     }
 }
 
