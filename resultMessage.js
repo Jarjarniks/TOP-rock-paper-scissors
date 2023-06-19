76442c48b4a0fe8091653730dcc4dc101f28c34c
Nikolai Lønberg <niklonberg@gmail.com>
2023-06-19 13:28:37 +0200

    Removed most of code for resultMsg class, previous attempts incorrect

--- resultMessage.js
+++ resultMessage.js
@@ -1,19 +1,18 @@
 class resultMsg {
-    constructor(playerSelection, computerSelection) {
-        this.playerSelection = playerSelection
-        this.computerSelection = computerSelection
+    constructor() {
+        
     }
 
     win() {
-        return `Player wins! player chose ${this.playerSelection}, computer chose ${this.computerSelection}`
+        return `Player wins! player chose ${playerSelection}, computer chose ${computerSelection}`
     }
     
     draw() {
-        return `Both participants used ${this.playerSelection} - this round is a draw`
+        return `Both participants used ${playerSelection} - this round is a draw`
     }
 
     lose() {
-        return `Player loses! player chose ${this.playerSelection}, computer chose ${this.computerSelection}`
+        return `Player loses! player chose ${playerSelection}, computer chose ${computerSelection}`
     }
 }
 
