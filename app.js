09ba35c25f05217711b1a755dfbd77cced5eaaad
Nikolai Lønberg <niklonberg@gmail.com>
2023-06-19 10:19:58 +0200

    Add random number gen. & return statement to getComputerChoice()

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const choices = ["Rock", "Paper", "Scissors"];
 
 function getComputerChoice() {
-    
-
+    const number = Math.floor(Math.random() * choices.length);
+    return choices[number]
 }
+
+console.log(getComputerChoice());
