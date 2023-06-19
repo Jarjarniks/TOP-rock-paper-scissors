9275dbf413e699ba05db2dad3ffcf2763bbbcc53
Nikolai Lønberg <niklonberg@gmail.com>
2023-06-19 11:04:31 +0200

    Remove singular if statement checking for same choice. Add switch statement instead

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-const choices = ["Rock", "Paper", "Scissors"];
+const choices = ["Rock", "Paper", "Scissor"];
 
 function getComputerChoice() {
     const number = Math.floor(Math.random() * choices.length);
@@ -9,14 +9,22 @@ function playRound(playerSelection, computerSelection) {
     playerSelection = playerSelection.toLowerCase();
     computerSelection = computerSelection.toLowerCase();
 
-    if (playerSelection === computerSelection) {
-        console.log(`Both of participants used ${playerSelection} - this round is a draw`)
+    switch(playerSelection) {
+        case "rock":
+            if (computerSelection === "scissor") {
+                console.log(`Player wins! player chose ${playerSelection}, computer chose ${computerSelection}`);
+            } else if (computerSelection === "rock") {
+                console.log(`Both participants used ${playerSelection} - this round is a draw`)
+            } else {
+                console.log(`Player loses! player chose ${playerSelection}, computer chose ${computerSelection}`);
+            }
     }
 }
 
 const playerSelection = "rock"
 const computerSelection = getComputerChoice();
 
-console.log(getComputerChoice());
+console.log(computerSelection);
+console.log(playerSelection)
 
 playRound(playerSelection, computerSelection);
