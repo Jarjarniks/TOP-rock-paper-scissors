ec26c15d47c50e64193dde6f96aa1677cac3e9fd
Nikolai Lønberg <niklonberg@gmail.com>
2023-06-27 14:46:02 +0200

    Remove unneeded endResultTie(), as it can never occur. Finish endResultWin. Minor grammar & syntax fixes

--- resultMessage.js
+++ resultMessage.js
@@ -6,36 +6,33 @@ class resultMsg {
     }
 
     roundWinMsg(playerSelection, computerSelection) {
-        return `Player wins! player chose ${playerSelection}, computer chose ${computerSelection}`
+        return `Player wins! player chose ${playerSelection}, computer chose ${computerSelection}`;
     }
     
     roundTieMsg(playerSelection) {
-        return `Both participants used ${playerSelection} - this round is a draw`
+        return `Both participants used ${playerSelection} - this round is a draw`;
     }
 
     roundLoseMsg(playerSelection, computerSelection) {
-        return `Player loses! player chose ${playerSelection}, computer chose ${computerSelection}`
+        return `Player loses! player chose ${playerSelection}, computer chose ${computerSelection}`;
     }
 
     updateRoundResult(msg) {
-        this.roundResultDiv.textContent = msg
+        this.roundResultDiv.textContent = msg;
     }
 
     updateCurrentScore(GAME) {
         this.currentScoreDiv.textContent = `The score is:
-        Player points: ${GAME.playerPoints}. Computer points: ${GAME.computerPoints}`
+        Player points: ${GAME.playerPoints}. Computer points: ${GAME.computerPoints}`;
     }
 
-    endResultWin() {
-        this.endResultDiv.textContent = 
+    endResultWin(GAME) {
+        this.currentScoreDiv.textContent = "";
+        this.endResultDiv.textContent = `You won! You got ${GAME.playerPoints}. The computer got ${GAME.computerPoints}`;
     }
 
-    endResultTie() {
+    endResultLose() {
 
-    }
-
-    endResutLose() {
-        
     }
 }
 
