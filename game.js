1eecf6a013b3098d258ca82649ff08fe6c8ff7f0
Nikolai Lønberg <niklonberg@gmail.com>
2023-06-27 14:50:06 +0200

    Add roundsPlayed variable to track rounds played to game class

--- game.js
+++ game.js
@@ -3,6 +3,7 @@ class game {
         this.gameType = gameType
         this.playerPoints;
         this.computerPoints;
+        this.roundsPlayed = 0;
     }
 
     startGame() {
@@ -11,6 +12,8 @@ class game {
     }
 
     updatePoints(resultObj) {
+        
+
         this.playerPoints += resultObj.playerPoints;
         this.computerPoints += resultObj.computerPoints;
     }
