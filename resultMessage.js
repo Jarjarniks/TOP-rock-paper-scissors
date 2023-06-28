1dac7691229fcc3fe5e425777ad7869651176b19
Nikolai Lønberg <niklonberg@gmail.com>
2023-06-28 11:14:51 +0200

    Refactored 3 round result methods into one roundResultMsg

--- resultMessage.js
+++ resultMessage.js
@@ -5,16 +5,8 @@ class resultMsg {
         this.endResultDiv = endResultDiv;
     }
 
-    roundWinMsg(playerSelection, computerSelection) {
+    roundResultMsg(playerSelection, computerSelection, message) {
         return `Player wins! player chose ${playerSelection}, computer chose ${computerSelection}`;
-    }
-    
-    roundTieMsg(playerSelection) {
-        return `Both participants used ${playerSelection} - this round is a draw`;
-    }
-
-    roundLoseMsg(playerSelection, computerSelection) {
-        return `Player loses! player chose ${playerSelection}, computer chose ${computerSelection}`;
     }
 
     updateRoundResult(msg) {
