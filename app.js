e70bc39f5ce9b74cdaa41bc05ffe83e503e5e732
Nikolai Lønberg <niklonberg@gmail.com>
2023-06-30 19:49:11 +0200

    Each time a button is clicked, all buttons get their class removed, then the class is added again to the clicked button

--- app.js
+++ app.js
@@ -122,6 +122,9 @@ setTimeout(() => {
 const buttons = document.querySelectorAll('button')
 buttons.forEach(button => {
     button.addEventListener('click', () => {
+        buttons.forEach(button => {
+            button.classList.remove('clicked')
+        })
         button.classList.add('clicked')
     })
 })
