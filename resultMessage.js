743fb99e8322d88c2a3db7b460b37571ff36a140
Nikolai Lønberg <niklonberg@gmail.com>
2023-07-03 11:28:41 +0200

    Message grammer fix

--- resultMessage.js
+++ resultMessage.js
@@ -7,28 +7,28 @@ class resultMsg {
         let message;
 
         if (playerPoints > computerPoints) {
-            message = `Player wins! player chose ${playerSelection}, computer chose ${computerSelection}`
+            message = `Player wins! player chose ${playerSelection}, the leprechaun chose ${computerSelection}.`
         } else if (playerPoints === computerPoints) {
-            message = `It's a tie! player chose ${playerSelection}, computer chose ${computerSelection}`
+            message = `It's a tie! player chose ${playerSelection}, the leprechaun chose ${computerSelection}.`
         } else {
-            message = `Player loses! player chose ${playerSelection}, computer chose ${computerSelection}`
+            message = `Player loses! player chose ${playerSelection}, the leprechaun chose ${computerSelection}.`
         }
         this.context.roundResultDiv.textContent = message
     }
 
     updateCurrentScore(GAME) {
         this.context.currentScoreDiv.textContent = `The score is:
-        Player points: ${GAME.playerPoints}. Computer points: ${GAME.computerPoints}`;
+        Player points: ${GAME.playerPoints}. Leprechaun points: ${GAME.computerPoints}.`;
     }
 
     endResultMsg(playerPoints, computerPoints) {
         let message;
         if (playerPoints > computerPoints) {
-            message = `You won! You got ${playerPoints} points. The computer got ${computerPoints} points`;
+            message = `You won! You got ${playerPoints} points. The leprechaun got ${computerPoints} points.`;
         } else if (playerPoints === computerPoints) {
-            message = `It's a tie! You got ${playerPoints} points. The computer got ${computerPoints} points. Time for a rematch?`;
+            message = `It's a tie! You got ${playerPoints} points. The leprechaun got ${computerPoints} points. Time for a rematch?`;
         } else {
-            message = `You lost! You got ${playerPoints} points. The computer got ${computerPoints} points`;
+            message = `You lost! You got ${playerPoints} points. The leprechaun got ${computerPoints} points.`;
         }
         this.context.currentScoreDiv.textContent = "";
         this.context.endResultDiv.textContent = message;
